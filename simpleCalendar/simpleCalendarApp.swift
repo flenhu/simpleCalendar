29a3804b36dde4b05b67fb7adb5eb23260ce6b14
flenhu <frank.lenhu@gmail.com>
2023-05-05 17:37:13 -0700

    Initial Commit

---
+++ simpleCalendar/simpleCalendarApp.swift
@@ -0,0 +1,17 @@
+//
+//  simpleCalendarApp.swift
+//  simpleCalendar
+//
+//  Created by Frank Le-Nhu on 5/5/23.
+//
+
+import SwiftUI
+
+@main
+struct simpleCalendarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
